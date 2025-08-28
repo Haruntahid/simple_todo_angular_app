905580f81bdc33693ea1be655b9c7d48f7a4ccdf
haruntahid <haruntahid3660@gmail.com>
2025-08-28 13:21:21 +0600

    add encode support

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,30 +14,58 @@ import {NgForOf} from "@angular/common";
 })
 export class HomeComponent implements OnInit {
 
-
-
   todoText: string = '';               // Input value
-  todoItems: { text: string }[] = [];  // Array of todos
+  todoItems: { text: string }[] = []; // Array of todos
+  encodeString = "";
+
+
+
+
 
   ngOnInit() {
     this.getData();
   }
 
+  getEncodedText(){
+    const name = "tahid";
+    let encodeText = "";
+    encodeText = btoa(name);
+    console.log(encodeText);
+    console.log(atob(encodeText));
+  }
+
+
+  // @ts-ignore
+  encode(arr : string) : string {
+    return btoa(arr);
+  }
+
+  // @ts-ignore
+  decode(arr : string) : string {
+    // @ts-ignore
+    return atob(arr);
+  }
+
+
+
   // Add a new todo
   addTodo() {
     if (this.todoText.trim()) {
       this.todoItems.push({ text: this.todoText });
-      // @ts-ignore
-      localStorage.setItem('todoArray', JSON.stringify(this.todoItems));
-      this.todoText = ''; // Clear input after adding
+
+      localStorage.setItem('todoArray', this.encode(JSON.stringify(this.todoItems)));
+      this.todoText = '';
     }
   }
 
   getData(){
     // @ts-ignore
     const data = localStorage.getItem('todoArray');
+
     if(data){
-      this.todoItems = JSON.parse(data);
+      let decodedJson = this.decode(data)
+      let parseJson = JSON.parse(decodedJson);
+      this.todoItems = Array.from(parseJson);
     }else {
       this.todoItems = [];
     }
@@ -47,17 +75,20 @@ export class HomeComponent implements OnInit {
 
   // Delete a todo by index
   deleteTodo(index: number) {
+    console.log(index);
     this.todoItems.splice(index, 1);
-    // @ts-ignore
-    localStorage.setItem('todoArray', JSON.stringify(this.todoItems))
+
+    localStorage.setItem('todoArray', this.encode(JSON.stringify(this.todoItems)));
   }
 
   // Edit a todo
   editTodo(index: number) {
+    console.log(index);
     const newText = prompt('Edit your task:', this.todoItems[index].text);
     if (newText !== null && newText.trim() !== '') {
       this.todoItems[index].text = newText.trim();
-      localStorage.setItem('todoArray', JSON.stringify(this.todoItems));
+
+      localStorage.setItem('todoArray', this.encode(JSON.stringify(this.todoItems)));
     }
   }
 
