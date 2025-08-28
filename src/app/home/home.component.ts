43ca5311727d370006ba3767e8ce521a37157829
haruntahid <haruntahid3660@gmail.com>
2025-08-28 10:34:58 +0600

    add data on localstorage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {NgForOf} from "@angular/common";
 
@@ -12,22 +12,44 @@ import {NgForOf} from "@angular/common";
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+
 
   todoText: string = '';               // Input value
   todoItems: { text: string }[] = [];  // Array of todos
+
+  ngOnInit() {
+    this.getData();
+  }
 
   // Add a new todo
   addTodo() {
     if (this.todoText.trim()) {
       this.todoItems.push({ text: this.todoText });
+      // @ts-ignore
+      localStorage.setItem('todoArray', JSON.stringify(this.todoItems));
       this.todoText = ''; // Clear input after adding
     }
   }
 
+  getData(){
+    // @ts-ignore
+    const data = localStorage.getItem('todoArray');
+    if(data){
+      this.todoItems = JSON.parse(data);
+    }else {
+      this.todoItems = [];
+    }
+  }
+
+
+
   // Delete a todo by index
   deleteTodo(index: number) {
     this.todoItems.splice(index, 1);
+    // @ts-ignore
+    localStorage.setItem('todoArray', JSON.stringify(this.todoItems))
   }
 
   // Edit a todo
@@ -35,6 +57,7 @@ export class HomeComponent {
     const newText = prompt('Edit your task:', this.todoItems[index].text);
     if (newText !== null && newText.trim() !== '') {
       this.todoItems[index].text = newText.trim();
+      localStorage.setItem('todoArray', JSON.stringify(this.todoItems));
     }
   }
 
